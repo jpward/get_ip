ee03a8c9678aedc87c73f52fe2aac26f2705fb3c
jpward <ubuntu@arm.localdomain>
2018-01-07 16:16:35 +0000

    Corrected path when running from systemd

--- get_ip.sh
+++ get_ip.sh
@@ -5,6 +5,9 @@
 #  https://askubuntu.com/questions/95910/command-for-determining-my-public-ip
 
 set -e
+
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
 OLD_IP="$(grep "IP=" mail.txt | sed "s/IP=//")"
 echo ${OLD_IP}
 
@@ -12,13 +15,13 @@ NEW_IP="$(nc 4.ifcfg.me 23 | grep IPv4 | cut -d' ' -f4 | sed 's/\r//')"
 echo ${NEW_IP}
 
 if ! [ "${OLD_IP}" = "${NEW_IP}" ]; then
-  cp mail.txt.orig mail.txt
+  cp ${HERE}/mail.txt.orig ${HERE}/mail.txt
 
-  sed -i "s/%IP%/${NEW_IP}/" mail.txt
+  sed -i "s/%IP%/${NEW_IP}/" ${HERE}/mail.txt
 
 
   curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
     --mail-from 'from@gmail.com' --mail-rcpt 'to@gmail.com' \
-    --upload-file mail.txt --user 'from@gmail.com:password' --insecure
+    --upload-file ${HERE}/mail.txt --user 'from@gmail.com:password' --insecure
 
 fi
