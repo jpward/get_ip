0fec3376403dc5399c6f67bbd7c8b89c4954a089
jpward <Ward.j2@Gmail.com>
2018-01-16 01:39:57 -0500

    Added check for failed IP retrieval, updated to use /tmp

--- get_ip.sh
+++ get_ip.sh
@@ -8,20 +8,30 @@ set -e
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-OLD_IP="$(grep "IP=" ${HERE}/mail.txt | sed "s/IP=//")"
+if ! [ -f /tmp/mail.txt ]; then
+  cp ${HERE}/mail.txt /tmp/mail.txt
+fi
+
+OLD_IP="$(grep "IP=" /tmp/mail.txt | sed "s/IP=//")"
 echo ${OLD_IP}
 
 NEW_IP="$(nc 4.ifcfg.me 23 | grep IPv4 | cut -d' ' -f4 | sed 's/\r//')"
-echo ${NEW_IP}
+
+#if a valid IP is found (invalid will be blank)
+if echo ${NEW_IP} | grep -q [0-9] ; then
+  echo ${NEW_IP}
+else
+  #Try again later
+  exit 0
+fi
 
 if ! [ "${OLD_IP}" = "${NEW_IP}" ]; then
-  cp ${HERE}/mail.txt.orig ${HERE}/mail.txt
+  cp ${HERE}/mail.txt.orig /tmp/mail.txt
 
-  sed -i "s/%IP%/${NEW_IP}/" ${HERE}/mail.txt
-
+  sed -i "s/%IP%/${NEW_IP}/" /tmp/mail.txt
 
   curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
     --mail-from 'from@gmail.com' --mail-rcpt 'to@gmail.com' \
-    --upload-file ${HERE}/mail.txt --user 'from@gmail.com:password' --insecure
+    --upload-file /tmp/mail.txt --user 'from@gmail.com:password' --insecure
 
 fi
